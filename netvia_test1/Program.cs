fa4bb7b3a92a5c8e5a3b5654e24c754b0abaae5e
carl <305522955@qq.com>
2019-12-08 16:12:43 +0800

    修改了实现文件

--- netvia_test1/Program.cs
+++ netvia_test1/Program.cs
@@ -8,8 +8,43 @@ namespace netvia_test1
 {
     class Program
     {
+        public static string Name
+        {
+            get;
+            set;
+            
+        }
         static void Main(string[] args)
         {
+        
+            var data = new { test = "test", a = 10 };
+            var data2 = new { test = "test2", a = 20 };
+            data = data2;
+            Name = "carl";
+            Console.WriteLine(Name);
+            Console.WriteLine(data.Equals(data2));
+            Console.WriteLine(data.test);
+            Test("test", 1, 2);
+        }
+
+        public static void Test(params Object[] data)
+        {
+            foreach(var item in data)
+            {
+                Console.WriteLine(item);
+            }
+            string a = "est";
+            string b = "ok";
+            Swap(ref a, ref b);
+            Console.WriteLine(a + " " + b);
+            Console.ReadKey();
+        }
+
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            T tmp = a;
+            a = b;
+            b = tmp;
         }
     }
 }
